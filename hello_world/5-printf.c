04a71df3d5fe27fdddc4d1d7220a0bd8564014c7
Duncan MacLennan <6733@holbertonstudents.com>
2023-06-16 23:48:23 -0700

    5-printf.c

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-/**
+/*
  * main - Entry point
  *
  * Description: 'the program's description'
@@ -7,8 +7,8 @@
  * Return: Always 0 (Success)
  */
 
-int main() {
+int main(void)
+{
 printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
-
