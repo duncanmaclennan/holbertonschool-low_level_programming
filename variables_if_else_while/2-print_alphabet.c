7f8ab76c09db7cfb6735f721e52cc5b931b3203d
Duncan MacLennan <6733@holbertonstudents.com>
2023-06-18 22:36:45 -0700

    2-print_alphabet.c

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,1 +1,14 @@
-wewee
+#include <stdio.h>
+/**
+ * main: This is a description
+ * Return: This is the return action
+ */
+int main(void) {
+    char ch = 'a';
+    while (ch <= 'z') {
+        putchar(ch);
+        ch = ch+1;
+    }
+    putchar('\n');
+    return 0;
+}
