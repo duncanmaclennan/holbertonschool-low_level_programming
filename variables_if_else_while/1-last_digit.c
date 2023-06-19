03b474dcb2b106143d3fa509eb1cf97a66a5109d
Duncan MacLennan <6733@holbertonstudents.com>
2023-06-18 20:38:37 -0700

    1-last_digit.c

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -8,12 +8,10 @@
 
 int main(void)
 {
-
-
+        int n;
+        int lastDigit = abs(n) % 10;
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-        int n;
-	int lastDigit = abs(n) % 10;
 
 	printf("Last digit of %d is ", n);
 
