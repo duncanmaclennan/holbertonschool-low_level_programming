433fc60ce3ed4624bb2f2470ae4dd41afad22b12
Duncan MacLennan <6733@holbertonstudents.com>
2023-06-18 21:25:58 -0700

    1-last_digit.c

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -12,19 +12,6 @@ int main(void)
         srand(time(0));
         n = rand() - RAND_MAX / 2;
 
-        // int printf(const char *format, ...);
-
-        // printf("Last digit of %d is ", n);
-
-        // if (lastDigit > 5) {
-        //         printf("%d and is greater than 5", lastDigit);
-        // } else if (lastDigit == 0) {
-        //         printf("%d and is 0", lastDigit);
-        // } else {
-        //         printf("%d and is less than 6 and not 0", lastDigit);
-        // }
-
-        // printf("\n");
 
         return (0);
 }
