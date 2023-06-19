c47ed9ab90e890ec533e317ce524cd1ac788e4e7
Duncan MacLennan <6733@holbertonstudents.com>
2023-06-18 22:01:04 -0700

    1-last_digit.c

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -18,6 +18,10 @@ int main(void)
 
         if (lastDigit > 5) {
                 printf("%d and is greater than 5", lastDigit);
+        } else if (lastDigit == 0) {
+                printf("%d and is 0", lastDigit);
+        } else {
+                printf("%d and is less than 6 and not 0", lastDigit);
         }
 
 
