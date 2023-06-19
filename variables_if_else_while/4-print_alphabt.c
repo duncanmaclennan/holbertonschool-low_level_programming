a65ff5e4e78317c6f0f542c27230b4594be970a0
Duncan MacLennan <6733@holbertonstudents.com>
2023-06-18 23:32:20 -0700

    4-print_alphabt.c

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -5,13 +5,14 @@
  */
 int main(void)
 {
-char ch;
-for (ch = 'a'; ch <= 'z'; ch++)
+char ch = 'a';
+while (ch <= 'z')
 {
 if (ch != 'q' && ch != 'e')
 {
 putchar(ch);
 }
+ch=ch+1;
 }
 putchar('\n');
 return (0);
