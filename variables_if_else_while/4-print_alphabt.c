b1a87acacf728e88e37edd19ef02cb6bac4da092
Duncan MacLennan <6733@holbertonstudents.com>
2023-06-18 23:26:02 -0700

    4-print_alphabt.c

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - This is a description
+ * Return: This is the return action
+ */
+int main(void) {
+char ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+if (ch != 'q' && ch != 'e')
+{
+putchar(ch);
+}
+}
+putchar('\n');
+return 0;
+}
