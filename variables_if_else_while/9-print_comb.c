0c2972f63a008ddbe09c52d2bcb853e3c98967fa
Duncan MacLennan <6733@holbertonstudents.com>
2023-06-19 15:53:22 -0700

    9-print_comb.c

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -1,19 +1,23 @@
 #include <stdio.h>
+/**
+ * main - This is a description
+ * Return: This is the return action
+ */
+int main(void)
+{
+int num = 0;
+while (num < 10)
+{
+putchar('0' + num);
+if (num != 9)
+{
+putchar(',');
+putchar(' ');
+}
+num = num + 1;
+}
 
-int main(void) {
-    int num = 0;
-    while (num < 10) {
-        int num2 = 0;
-        while (num2 < 10) {
-            putchar('0' + num);
-            putchar('0' + num2++);
-            if (num2 != 10) {
-                putchar(',');
-                putchar(' ');
-            }
-        }
-        num++;
-    }
+putchar('\n');
 
-    return 0;
+return (0);
 }
