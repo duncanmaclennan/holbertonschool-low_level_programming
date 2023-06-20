277f43892b66e1441cf04c2dfe9efc9cca90a080
Duncan MacLennan <6733@holbertonstudents.com>
2023-06-19 23:34:14 -0700

    4-isalpha.c

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -7,16 +7,15 @@
  */
 int _isalpha(int c)
 {
-	if (c >= 'a' && c <= 'z')
+if (c >= 'a' && c <= 'z')
 {
-	return (1);
+return (1);
 }
-	else if (c >= 'A' && c <= 'Z')
-	      {
-		      return (1);
-	      }
-	else
-	{
-	return (0);
+else if (c >= 'A' && c <= 'Z')
+{
+return (1);	      }
+else
+{
+return (0);
 }
 }
