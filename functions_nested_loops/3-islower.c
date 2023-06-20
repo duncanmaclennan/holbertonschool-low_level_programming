485adecac431fe2a5dd0788c1ea1053e78857148
Duncan MacLennan <6733@holbertonstudents.com>
2023-06-19 22:51:18 -0700

    3-islower.c

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - This is a description
+ * Return: This is the return action
+ */
+int _islower(int c)
+{
+if (c > 96)
+{
+	return(1);
+}
+else
+{
+return (0);
+}
+}
