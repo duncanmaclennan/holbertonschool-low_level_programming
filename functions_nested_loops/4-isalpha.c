db5d710017ab01b7936f3b3f4111f43d19c9ef7e
Duncan MacLennan <6733@holbertonstudents.com>
2023-06-19 23:22:25 -0700

    4-isalpha.c

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks for alphabetic character
+ * @c - the character passed to function
+ * returns - 1 if alphabetic, 0 if not
+ */
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+{
+	return (1);
+}
+else
+{
+	return (0);
+}
+}
