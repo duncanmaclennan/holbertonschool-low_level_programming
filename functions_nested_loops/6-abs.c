7b4d29f5c9fe0256176e66e05ef4ade47d72329b
Duncan MacLennan <6733@holbertonstudents.com>
2023-06-20 00:07:20 -0700

    6-abs.c

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _abs - This is a description
+ * @n:  this is the character passed
+ * Return: returns 1 if true, 0 if false
+ */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (-n);
+	}
+	else
+	{
+		return (n);
+	}
+}
