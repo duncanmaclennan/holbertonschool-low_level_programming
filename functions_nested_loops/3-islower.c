998b8e826b26a4a5d1079efedc5c46efd13dd225
Duncan MacLennan <6733@holbertonstudents.com>
2023-06-19 23:09:07 -0700

    3-islower.c

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * _islower - This is a description
- * c:  this is the character passed
+ * @c:  this is the character passed
  * Return: returns 1 if true, 0 if false
  */
 int _islower(int c)
