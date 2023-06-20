bc5457638d1f2667b5e9a4d1214f2fb89b64b36f
Duncan MacLennan <6733@holbertonstudents.com>
2023-06-19 18:06:55 -0700

    0-putchar.c

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - This is a description
+ * Return: This is the return action
+ */
+int main(void) {
+putchar('_');
+putchar('p');
+putchar('u');
+putchar('t');
+putchar('c');
+putchar('h');
+putchar('a');
+putchar('r');
+putchar('\n');
+
+return (0);
+}
