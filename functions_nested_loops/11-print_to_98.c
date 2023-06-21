b402d6d0abfb9e84142621c6bfac9672d25015cb
Duncan MacLennan <6733@holbertonstudents.com>
2023-06-20 23:50:38 -0700

    11-print_to_98.c

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -12,8 +12,16 @@ void print_to_98(int n)
 	{
 		while (n >= 98)
 		{
-			printf("%d, ", n);
-			n = n - 1;
+			if (n > 98)
+			{
+				printf("%d, ", n);
+				n = n - 1;
+			}
+			else
+			{
+				printf("%d", n);
+				n = n - 1;
+			}
 		}
 		printf("\n");
 	}
