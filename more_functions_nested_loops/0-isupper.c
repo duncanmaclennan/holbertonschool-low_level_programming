1f59d4f2f619cd1a76dcfb244f87d009a0cb0381
Duncan MacLennan <6733@holbertonstudents.com>
2023-06-24 00:59:23 -0700

    0-isupper.c

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,11 +1,18 @@
+#include "main.h"
+/**
+ * _isupper - tests if character uppercase
+ * @c: the character
+ * Return: 1 if true, 0 if false
+ */
+
 int _isupper(int c)
 {
-    if (c >= 'A' && c <= 'Z')
-    {
-	    return 1;
-    }
-    else
-    {
-	    return 0;
-    }
+if (c >= 'A' && c <= 'Z')
+{
+	return (1);
+}
+else
+{
+	return (0);
+}
 }
