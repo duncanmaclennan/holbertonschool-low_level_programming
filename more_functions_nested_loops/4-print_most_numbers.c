814fb2fca4f4801fb91defc826d2087ba768cccb
Duncan MacLennan <6733@holbertonstudents.com>
2023-06-24 16:49:30 -0700

    4-print_most_numbers.c

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_most_numbers - tests if character is digit
+ * Return: the output
+ */
+void print_most_numbers(void)
+{
+	int n;
+
+	n = 0;
+	while (n < 10)
+	{
+		if (n != 2 && n != 4)
+		{
+			_putchar('0' + n);
+		}
+		n = n + 1;
+	}
+	_putchar('\n');
+}
