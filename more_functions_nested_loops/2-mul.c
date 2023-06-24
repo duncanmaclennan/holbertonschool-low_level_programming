2803c1bc70376c56c60fd6e258d0f5437a0fc301
Duncan MacLennan <6733@holbertonstudents.com>
2023-06-24 16:16:52 -0700

    2-mul.c

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * mul - tests if character is digit
+ * @a: the first integer
+ * @b: the second integer
+ * Return: 1 if true, 0 if false
+ */
+
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+
+	return (c);
+}
