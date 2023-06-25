dc815e70b00be002424318ed90a12e41ad4856f1
Duncan MacLennan <6733@holbertonstudents.com>
2023-06-25 15:40:50 -0700

    tri

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -1,25 +1,30 @@
 #include "main.h"
 /**
- * fizz_buzz - tests if character is digit
- * main: runs the program
+ * print_triangle - tests if character is digit
+ * @size: runs the program
  * Return: the output
  */
-void print_triangle(int size) {
+void print_triangle(int size)
+{
 	int i;
 	int j;
 
-	if (size <= 0) {
+	if (size <= 0)
+	{
 		_putchar('\n');
 		return;
 	}
 
-    for (i = 1; i <= size; i++) {
-        for (j = 1; j <= size - i; j++) {
-            _putchar(' ');
-        }
-        for (j = 1; j <= i; j++) {
-            _putchar('#');
-        }
-        _putchar('\n');
-    }
+for (i = 1; i <= size; i++)
+{
+	for (j = 1; j <= size - i; j++)
+	{
+		_putchar(' ');
+	}
+	for (j = 1; j <= i; j++)
+	{
+		_putchar('#');
+	}
+	_putchar('\n');
+}
 }
