4040ae3c4d161383e296ae568a9c71d8dbcfd26c
Duncan MacLennan <6733@holbertonstudents.com>
2023-06-25 23:51:08 -0700

    4 rev

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_rev - tests if character is digit
+ * @s: the first integer
+ */
+void print_rev(char *s)
+{
+	char *end = s;
+	while (*end != '\0')
+	{
+		end++;
+	}
+	end--;
+	while (end >= s)
+	{
+		_putchar(*end);
+		end--;
+	}
+	_putchar('\n');
+}
