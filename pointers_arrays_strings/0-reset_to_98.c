50e2360675907299b74d035bf5d1e4be9f7e3dcc
Duncan MacLennan <6733@holbertonstudents.com>
2023-06-25 21:11:22 -0700

    0-reset_to_98.c

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * reset_to_98 - tests if character is digit
+ * @n: the integer
+ */
+
+void reset_to_98(int *n)
+{
+    *n = 98;
+}
