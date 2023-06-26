30e44e16047fb37f02ce8b1c2066a5e64e4fa08c
Duncan MacLennan <6733@holbertonstudents.com>
2023-06-26 15:28:09 -0700

    7-puts_half.c

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * puts_half - tests if character is digit
+ * @str: the first integer
+ */
+void puts_half(char *str)
+{
+	int length = 0;
+	int i;
+	int start;
+	int end;
+
+	while (str[length] != '\0')
+	{
+		length++;
+	}
+
+	start = (length) / 2;
+	end = length - 1;
+
+	for (i = start; i <= end; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
