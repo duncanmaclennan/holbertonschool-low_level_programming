8851156003c1a23910eb1bacab4dccd4740fd26b
Duncan MacLennan <6733@holbertonstudents.com>
2023-07-10 16:50:34 -0700

    2-str_concat.c

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -7,13 +7,15 @@
  * Return: the pointer
  */
 
-char *str_concat(char *s1, char *s2) {
+char *str_concat(char *s1, char *s2)
+{
 
 	int len1;
 	int len2;
 	int i;
 	int j;
 	char *concatenated;
+
 	if (s1 == NULL)
 	{
 		s1 = "";
