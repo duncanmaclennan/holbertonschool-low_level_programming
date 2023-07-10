164ac0b91fe7560a76b2c2284535dc95d4454784
Duncan MacLennan <6733@holbertonstudents.com>
2023-07-09 19:18:05 -0700

    malloc

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * create_array - prints a string
+ * @size: location of the string in memory
+ * @c: the character
+ * Return: the conversion
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *array;
+	unsigned int i;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	array = (char *)malloc(sizeof(char) * size);
+	if (array == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < size; i++)
+	{
+		array[i] = c;
+	}
+	return (array);
+}
