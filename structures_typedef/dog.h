ff9de46b410063df064e23179325126c4a7f9762
Duncan MacLennan <6733@holbertonstudents.com>
2023-07-18 19:59:25 -0700

    dog.h

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,12 @@
 #ifndef DOG_H
 #define DOG_H
-
+/**
+ * struct dog - structure for dog
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ * Description: data structure for dog
+ */
 struct dog
 {
 	char *name;
