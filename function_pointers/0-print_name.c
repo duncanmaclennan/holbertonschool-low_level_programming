fc47f4135abd99148a0eadf185086b5d4880ed69
Duncan MacLennan <6733@holbertonstudents.com>
2023-07-19 22:05:50 -0700

    0-print_name

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,6 +1,11 @@
 #include <stdlib.h>
 #include "function_pointers.h"
-
+/**
+ *print_name - prints a name
+ *@name: the name
+ *@f: the pointer
+ *Return: nothing
+ */
 void print_name(char *name, void (*f)(char *))
 {
 	if (name == NULL || f == NULL)
