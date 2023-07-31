a941897cc20040956ccad3a4f8394a05bacdfa8b
Duncan MacLennan <6733@holbertonstudents.com>
2023-07-30 19:59:50 -0700

    revised lists.h

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -1,10 +1,19 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-typedef struct list_s {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+/**
+ * struct list_s - structure for dog
+ * @str: dog's name
+ * @len: dog's age
+ * @next: dog's owner
+ * Description: list struct
+ */
+
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
 } list_t;
 
 size_t print_list(const list_t *h);
