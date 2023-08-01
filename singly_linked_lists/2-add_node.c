75659ee4a69a4138e8f7fda673d7867a500c6611
Duncan MacLennan <6733@holbertonstudents.com>
2023-07-31 18:37:32 -0700

    2-add_node.c

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <strings.h>
+#include "lists.h"
+
+/**
+ * add_node - A function prints a list
+ * @head: the list
+ * @str: the string
+ * Return: the count
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+
+	list_t *new_node = malloc(sizeof(list_t));
+
+	if (!new_node)
+	{
+		return (NULL);
+	}
+	new_node->str = strdup(str);
+	if (!new_node->str)
+	{
+		free(new_node);
+		return (NULL);
+	}
+
+	new_node->len = strlen(str);
+	new_node->next = *head;
+
+	*head = new_node;
+
+	return (new_node);
+}
