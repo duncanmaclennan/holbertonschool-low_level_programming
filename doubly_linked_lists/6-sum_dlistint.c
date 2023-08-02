e4b5b4aa3fdb087078a1707ab83ab7aeebce16bf
Duncan MacLennan <6733@holbertonstudents.com>
2023-08-01 23:40:48 -0700

    6-sum_dlistint.c

---
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * sum_dlistint - sums nodes
+ * @head: the list
+ */
+
+int sum_dlistint(dlistint_t *head)
+{
+	int sum;
+
+	sum = 0;
+	while (head)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+
+	return (sum);
+}
