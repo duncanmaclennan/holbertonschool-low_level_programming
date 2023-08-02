6e2de48593894774b3af97a9dd0e24d9a4e5620a
Duncan MacLennan <6733@holbertonstudents.com>
2023-08-01 23:14:09 -0700

    5-get_dnodeint.c

---
+++ doubly_linked_lists/#4-free_dlistint.c#
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_dlistint - frees a list from memory
+ * @head: the list
+ */
+
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *temp;
+
+	while (head)
+	{
+		temp = head;
+		head = head->next;
+
+		free(temp);
+	}
+}
---
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - frees a list from memory
+ * @head: the list
+ * @index: the list
+ * Return: the node
+ */
+
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int count;
+
+	count = 0;
+	while (head)
+	{
+		if (count == index)
+		{
+			return (head);
+		}
+		head = head->next;
+		count++;
+	}
+
+	return (NULL);
+}
