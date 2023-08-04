d59be858e39180070333e6a75dc582d7840482b6
Duncan MacLennan <6733@holbertonstudents.com>
2023-08-03 21:57:16 -0700

    4-clear_bit.c

---
+++ bit_manipulation/4-clear_bit.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * clear_bit - sets value of bit at index
+ * @n: pointer to the binary
+ * @index: the index
+ */
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int mask;
+
+	if (index >= sizeof(unsigned long int) * 8)
+	{
+		return (-1);
+	}
+
+	mask = ~(1UL << index);
+
+	*n = *n & mask;
+
+	return (1);
+}
