9195370f11c0424d263e010f4c9419364f349919
Duncan MacLennan <6733@holbertonstudents.com>
2023-08-06 21:14:54 -0700

    1-create_file.c

---
+++ file_io/1-create_file.c
@@ -0,0 +1,45 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+/**
+ * create_file - creates a file
+ * @filename: name of file
+ * @text_content: the letters
+ * Return: 1 if success
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	ssize_t write_count;
+	size_t length;
+
+	length = 0;
+
+	if (!filename)
+	{
+		return (-1);
+	}
+
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (fd == -1)
+	{
+		return (-1);
+	}
+
+	if (text_content)
+	{
+		while (text_content[length])
+		{
+			length++;
+		}
+		write_count = write(fd, text_content, length);
+		if (write_count == -1 || write_count != (ssize_t)length)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
+	close(fd);
+	return (1);
+}
